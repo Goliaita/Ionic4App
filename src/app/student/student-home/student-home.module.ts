bb423f9514fa4ac197b9ad85a13b6336f04da5af
Davide <davide.basile01@gmail.com>
2019-01-29 17:20:44 +0100

    just a little part of menu its on the way

--- src/app/student/student-home/student-home.module.ts
+++ src/app/student/student-home/student-home.module.ts
@@ -14,15 +14,6 @@ const routes: Routes = [
     path: '',
     component: StudentHomePage
   },
-  {
-    path: 'teaching-material',
-    component: TeachingFilesPage
-  },
-  {
-   path: 'chat-page',
-   component: ChatPagePage
-  }
-
 ];
 
 @NgModule({
